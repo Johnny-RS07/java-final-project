45b167c1506b7890a68c459346fc220a3be470ae
Johnny-RS07 <jrsalt182@rbc.edu>
2026-04-28 12:40:46 -0400

    Update TriviaGame.java

--- src/TriviaGame.java
+++ src/TriviaGame.java
@@ -1,6 +1,8 @@
 
 import java.util.Scanner;
 
+
+
 public class TriviaGame extends Game{
 	
 	
@@ -8,7 +10,7 @@ public class TriviaGame extends Game{
         super(name);
     }
 	
-	boolean done = false;
+	
 	
 	String [] questions = {"How many continents are there?", "What does the itlian word Farfalle mean?", "What dye came from snails?", "What three colors make up all other colors?", "What is the main way to remove wrinkles in clothes?", "What was the name of the cat and mouse in the famous 1940's cartoon?"};
 	String [] QA1 = {"A.7", "B.8", "C.5"};
@@ -28,7 +30,7 @@ public class TriviaGame extends Game{
 
     @Override
     public void play() {
-        runTriviaGame();
+        runTriviaGame(questions,QA1,QA2,QA3,QA4,QA5,QA6,correctAnswers);
     }
 	
 	public static void displayQuestion(String question, String [] QC) {
@@ -39,16 +41,14 @@ public class TriviaGame extends Game{
 	}
 	
 	public static String getUserAnswer() {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         String answer;
-
-        while (done = false) {
+        while (true) {
             System.out.print("Enter A, B, or C: ");
-            answer = scanner.nextLine();
+            answer = input.nextLine();
             answer = answer.toUpperCase();
 
             if (answer.equals("A") || answer.equals("B") || answer.equals("C")) {
-            	done = true;
                 return answer;
             } 
             else {
@@ -62,33 +62,33 @@ public class TriviaGame extends Game{
     }
     
     
-    public static void runTriviaGame() {
+    public static void runTriviaGame(String [] questions, String [] QA1, String [] QA2, String [] QA3, String [] QA4,String [] QA5,String [] QA6,String [] correctAnswers) {
     	int correct = 0;
     	int incorrect = 0;
     	
     	for (int i = 0; i <questions.length; i++) {
     		if (i == 0) {
-    			displayQuestion(questions[i], CA1);
+    			displayQuestion(questions[i], QA1);
     		}
     		
     		else if (i == 1) {
-    			displayQuestion(questions[i], CA2);
+    			displayQuestion(questions[i], QA2);
     		}
     		
     		else if (i == 2) {
-    			displayQuestion(questions[i], CA3);
+    			displayQuestion(questions[i], QA3);
     		}
     		
     		else if (i == 3) {
-    			displayQuestion(questions[i], CA4);
+    			displayQuestion(questions[i], QA4);
     		}
     		
     		else if (i == 4) {
-    			displayQuestion(questions[i], CA5);
+    			displayQuestion(questions[i], QA5);
     		}
     		
     		else if (i == 5) {
-    			displayQuestion(questions[i], CA6);
+    			displayQuestion(questions[i], QA6);
     		}
     		
     		String userInput = getUserAnswer();
